025264f3a72089e6052f058a9b63674461daebdc
Tristan Kirwan <0944035@hr.nl>
2021-11-13 20:02:52 +0100

    Protected login and register page

--- frontend/pages/login/index.js
+++ frontend/pages/login/index.js
@@ -1,7 +1,26 @@
-import LogincSection from '../../sections/loginSection/loginSection'
+import cookies from 'next-cookies';
+import LoginSection from '../../sections/loginSection/loginSection'
+
+export async function getServerSideProps(context) { 
+  const { accessToken } = cookies(context)
+  const { res } = context
+  let shouldRedirect = false;
+  if(accessToken) {
+    shouldRedirect = true
+  }
+
+  if(shouldRedirect){
+    res.writeHead(307, {
+      Location: '/account',
+    })
+    res.end();
+  }
+
+  return {props: {}}
+}
 
 export default function Login(){
   return(
-    <LogincSection />
+    <LoginSection />
   )
 }
--- frontend/pages/register/index.js
+++ frontend/pages/register/index.js
@@ -1,4 +1,23 @@
+import cookies from 'next-cookies';
 import RegisterSection from '../../sections/registerSection/registerSection';
+
+export async function getServerSideProps(context) { 
+  const { accessToken } = cookies(context)
+  const { res } = context
+  let shouldRedirect = false;
+  if(accessToken) {
+    shouldRedirect = true
+  }
+
+  if(shouldRedirect){
+    res.writeHead(307, {
+      Location: '/account',
+    })
+    res.end();
+  }
+
+  return {props: {}}
+}
 
 export default function Register(){
   return (
