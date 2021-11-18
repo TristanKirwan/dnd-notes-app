d298f88eafabdfb7d8c1d4d29feee0aae0bc1f34
Tristan Kirwan <0944035@hr.nl>
2021-11-18 13:36:48 +0100

    Changes to campaign overview page

--- frontend/sections/campaignOverviewSection/campaignOverviewSection.jsx
+++ frontend/sections/campaignOverviewSection/campaignOverviewSection.jsx
@@ -1,6 +1,11 @@
-import Container from '../../components/Container/container'
-import CampaignCard from '../../components/Cards/CampaignCard/campaignCard'
-import CampaignCreationForm from '../../components/Forms/campaignCreationForm/campaignCreationForm'
+import { useState } from 'react';
+import clsx from 'clsx'
+
+import Container from '../../components/Container/container';
+import Button from '../../components/Button/button';
+import Link from '../../components/Link/link';
+import CampaignCard from '../../components/Cards/CampaignCard/campaignCard';
+import CampaignCreationForm from '../../components/Forms/campaignCreationForm/campaignCreationForm';
 
 import style from './campaignOverviewSection.module.scss';
 
@@ -10,18 +15,32 @@ function NoCampaignsMarkup(){
       <p>
         You have been resting at the inn too long without any adventures! Get started By creating a campaign below.
       </p>
-      <CampaignCreationForm />
     </div>
   )
 }
 
 export default function CampaignOverviewSection({campaigns}){
+  const [showCreationForm, setShowCreationForm] = useState(false)
+
   return (
     <section className={style.campaignOverviewSection}>
-      <Container containerClass={style.container}>
-        {!Array.isArray(campaigns) || campaigns.length === 0 && <NoCampaignsMarkup />}
-        {Array.isArray(campaigns) && campaigns.map(campaign => <CampaignCard title={campaign.title} users={campaign.users} id={campaign.id}/>)}
-      </Container>
+      {showCreationForm ? 
+        <div>
+          <Container containerClass={style.container}>
+            <Link onClick={() => setShowCreationForm(false)} linkClass={style.backToOverviewButton}>
+              <i className={clsx(["fas fa-long-arrow-alt-left", style.arrow])}></i>Go back
+            </Link>
+            <CampaignCreationForm />
+          </Container>
+        </div>
+      : 
+        <Container containerClass={clsx([style.container, style.campaignCardContainer])}>
+          {!Array.isArray(campaigns) || campaigns.length === 0 && <NoCampaignsMarkup />}
+          {Array.isArray(campaigns) && campaigns.map(campaign => <CampaignCard title={campaign.title} users={campaign.users} id={campaign.id}/>)}
+          <div className={style.buttonContainer}>
+            <Button callBack={() => setShowCreationForm(true)}>Create a campaign</Button>
+          </div>
+      </Container>}
     </section>
   )
 }
