52fc491a0f7e61a8fcf60928388b5b0ab844ae7a
Tristan Kirwan <0944035@hr.nl>
2021-12-10 10:58:19 +0100

    Extracted delete Modal into its own component

---
+++ frontend/components/Modal/deleteModal/deleteModal.jsx
@@ -0,0 +1,51 @@
+import clsx from 'clsx';
+
+import Modal from '../modal';
+import Button from '../../Button/button';
+
+import style from './deleteModal.module.scss'
+
+export default function DeleteModal({
+  toggleDeleteModal,
+  deleteFunction,
+  deleteError,
+  headerText,
+  itemName,
+  bodyText
+  }){
+  // const [deleteError, setDeleteError] = useState(null)
+
+
+  // function deleteCampaign(){
+  //   setDeleteError(null)
+  //   makeAuthorizedRequest(`campaign/${campaignInfo.id}`, null, null, 'DELETE')
+  //   .then(() => {
+  //     setShowDeleteModal(false);
+  //     router.push('/account/campaigns')
+  //   })
+  //   .catch(() => {
+  //     setDeleteError('Something went wrong while deleting the campaign. Please try again later.')
+  //     console.error('Something went wrong while deleting the campaign.')
+  //   })
+  // }
+
+  return(
+    <Modal closeCallback={toggleDeleteModal}>
+        <span className={style.deleteModalHeader}>{headerText} 
+          <span className={style.modalCampaignTitle}> {itemName}</span>?
+        </span>
+        <p className={style.modalBody}>
+          {bodyText}
+        </p>
+        {deleteError && <p className={style.modalError}>{deleteError}</p>}
+        <div className={style.modalButtonContainer}>
+          <Button callBack={toggleDeleteModal} buttonClass={clsx([style.modalButton, style.cancelButton])}>
+            Cancel
+          </Button>
+          <Button buttonClass={clsx([style.deleteButton, style.modalButton])} callBack={deleteFunction}>
+            Delete
+          </Button>
+        </div>
+      </Modal>
+  )
+}
--- frontend/sections/campaignDetailSection/campaignDetailSection.jsx
+++ frontend/sections/campaignDetailSection/campaignDetailSection.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import clsx from 'clsx';
 import { useRouter } from 'next/router';
 
 import PageTitle from '../../components/PageTitle/pageTitle';
@@ -8,53 +7,17 @@ import Container from '../../components/Container/container';
 import CampaignCreationForm from '../../components/Forms/campaignCreationForm/campaignCreationForm';
 import CampaignDetailBlock from '../../components/CampaignDetailBlock/campaignDetailBlock'
 import CharactersBlock from '../../components/CharactersBlock/charactersBlock';
-import Modal from '../../components/Modal/modal';
-import Button from '../../components/Button/button';
+import DeleteModal from '../../components/Modal/deleteModal/deleteModal';
 
 import makeAuthorizedRequest from '../../utils/makeAuthorizedRequest';
 
 import style from './campaignDetailSection.module.scss';
 
-function DeleteModal({toggleDeleteModal, campaignInfo}){
-  const [deleteError, setDeleteError] = useState(null)
-
-
-  function deleteCampaign(){
-    setDeleteError(null)
-    makeAuthorizedRequest(`campaign/${campaignInfo.id}`, null, null, 'DELETE')
-    .then(() => {
-      setShowDeleteModal(false);
-      router.push('/account/campaigns')
-    })
-    .catch(() => {
-      setDeleteError('Something went wrong while deleting the campaign. Please try again later.')
-      console.error('Something went wrong while deleting the campaign.')
-    })
-  }
-
-  return(
-    <Modal closeCallback={toggleDeleteModal}>
-        <span className={style.deleteModalHeader}>Are you sure you want to delete <span className={style.modalCampaignTitle}>{campaignInfo.title}</span>?</span>
-        <p className={style.modalBody}>
-          This action cannot be undone and can only be performed by the dungeon master of the campaign.
-        </p>
-        {deleteError && <p className={style.modalError}>{deleteError}</p>}
-        <div className={style.modalButtonContainer}>
-          <Button callBack={toggleDeleteModal} buttonClass={clsx([style.modalButton, style.cancelButton])}>
-            Cancel
-          </Button>
-          <Button buttonClass={clsx([style.deleteButton, style.modalButton])} callBack={deleteCampaign}>
-            Delete
-          </Button>
-        </div>
-      </Modal>
-  )
-}
-
 export default function CampaignDetailSection(campaign) {
   const [isEditing, setIsEditing] = useState(false)
   const [campaignToShow, setCampaignToShow] = useState(null)
   const [showDeleteModal, setShowDeleteModal] = useState(null);
+  const [deleteModalError, setDeleteModalError] = useState(null)
   const router = useRouter();
 
   useEffect(() => {
@@ -72,9 +35,34 @@ export default function CampaignDetailSection(campaign) {
     setShowDeleteModal(!showDeleteModal)
   }
 
+  function deleteCampaign(){
+    setDeleteModalError(null)
+    makeAuthorizedRequest(`campaign/${campaignToShow.id}`, null, null, 'DELETE')
+    .then(() => {
+      setShowDeleteModal(false);
+      router.push('/account/campaigns')
+    })
+    .catch(() => {
+      setDeleteModalError('Something went wrong while deleting the campaign. Please try again later.')
+      console.error('Something went wrong while deleting the campaign.')
+    })
+  }
+
+  const deleteModalHeaderText = "Are you sure you want to delete";
+  const deleteModalItemName = campaignToShow ? campaignToShow.title : '';
+  const deleteModalText = "This action cannot be undone and can only be performed by the dungeon master of the campaign."
+
+
   return (
     <section className={style.campaignDetailSection}>
-      {showDeleteModal && <DeleteModal toggleDeleteModal={toggleDeleteModal} campaignInfo={campaignToShow}/>}
+      {showDeleteModal && <DeleteModal 
+        toggleDeleteModal={toggleDeleteModal}
+        deleteFunction={deleteCampaign}
+        deleteError={deleteModalError}
+        headerText={deleteModalHeaderText}
+        itemName={deleteModalItemName}
+        bodyText={deleteModalText}
+      />}
       {isEditing ? <Container containerClass={style.container}>
             <PageTitle>Editing: {campaignToShow.title}</PageTitle>
             <BackButton callBack={() => setIsEditing(false)}/>
